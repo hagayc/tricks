07ca6a63c9d83137edf1fe5e40c161c6d40a5530
hagayc <hagayc@tase.co.il>
2021-08-19 10:38:32 +0300

    feat(test): 2end

--- cloneProject.sh
+++ cloneProject.sh
@@ -7,7 +7,7 @@ chmod +x clone-repos.sh
  
 USER=XXXXXX
 PASS=XXXXXXX
-PROJECT=XXXXXX 
+PROJECT=XXXXXX∏
  
 curl -k -s -u "$USER:$PASS" https://XXX.XXX.XXX.XXX:8443/rest/api/1.0/projects/$PROJECT/repos/\?limit=1000\
 |jq -r '.values[] | {slug:.slug, links:.links.clone[] } |"git clone \(.links.href)"' \
